63c9716ac4aabce422bcd63521e2395979dc9e2d
menaserdoust <mehrdad.naser@gmail.com>
2019-06-13 22:03:20 -0400

    Added main.html

---
+++ myScript.js
@@ -0,0 +1,11 @@
+function changeButtonColor() {
+
+    if(document.getElementById("myButton").style.backgroundColor == 'red')
+    {
+      document.getElementById("myButton").style.backgroundColor = 'green';
+    }
+    else
+    {
+    document.getElementById("myButton").style.backgroundColor = 'red';
+    } 
+    }
